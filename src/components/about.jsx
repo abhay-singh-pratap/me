d4835013a1819b129e4cc50cbfb4403632db3159
Singh <157636@NTTDATA.COM>
2021-02-05 12:22:36 +0530

    changes

---
+++ src/components/about.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Me</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>I am an experienced IT professional with around 2 year of experience in Software Services and Product Development in agile environment. 
+                    I have a working experience with different clients from USA, Japan & India.
+                    I am also a computer grad student from Visvesvaraya Technological University with Computer Science and Engineering as my major. Currently I am pursuing Post Graducation  from International Institute of Information Technology Bangalore with Data Science as my major.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Full Stack development </h3>
+                    <p>I have experience building web based application using spring boot, Microservices, Amazon S3 bucket and Angular 8</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-code" />
+                </span>
+                <div className="desc">
+                    <h3>Backend Development/ API Development</h3>
+                    <p>I have also worked as an API developer for development of different pipelines using GO lang, PHP and Mysql</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Database Migration</h3>
+                    <p>I am also experienced in database migration and report generation using Java, Mysql and Orcale database.</p>
+                </div>
+                </div>
+            </div>
+          
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+
+                <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Software Development Analyst | NTT DATA<span> 2017-Present</span></h2>
+                       <p> I am working in NTT DATA since 2017 as a Software Development Analyst. I have been involved in API development, Data Migration and Full stack development processes.</p>
+                        </div>
+                    </div>
+                  </article>
+
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Certification | Orcale JAVA 8 <span> December 2016</span></h2>
+                        <p>I have done certification in Orcale JAVA 8 OCJA and OCJP.</p>
+                         </div>
+                    </div>
+                  </article>
+
+                  {/* <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Post Graduation | International Institute of Information Technology Bangalore<span> 2020-2021</span></h2>
+                       <p> I am pursuing Post Graducation from International Institute of Information Technology Bangalore with Data Science as my major.</p>
+                        </div>
+                    </div>
+                  </article> */}
+
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Under Graduation | AKTU, Lucknow <span> 2013-2017</span></h2>
+                       <p>I have done my undergraduation- Bachlore OF Engineering from AKTU with Computer Science and Engineering as my major. </p>
+                        </div>
+                    </div>
+                  </article>
+
+                  
+                  
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
